49b58fea2faf793100b964be578df706ffbe4da5
anggawanridho <anggawan.islami@ilcs.co.id>
2024-03-04 05:53:48 +0700

    fix(bug): adding cors in app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
-
+import cors from "cors";
 import indexRouter from "./routes/index.js";
 import usersRouter from "./routes/userRouter.js";
 
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(cors());
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
